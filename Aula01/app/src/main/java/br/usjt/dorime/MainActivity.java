f4c0179ba8a6cfdda052a2ceaf10eae0783d73c1
Ginsei <gabriel_fs96@hotmail.com>
2018-08-25 22:06:20 -0300

    First Commit

---
+++ Aula01/app/src/main/java/br/usjt/dorime/MainActivity.java
@@ -0,0 +1,33 @@
+package br.usjt.dorime;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+    //constante static para identifcar a mensaaem
+    public final static String EXTRA_MESSAGE =
+            "br.usjt.dorime.MESSAGE";
+
+    //será chamado quando o usuário clicar em Enviar. USE ALT + ENTER PARA IMPORT
+    public void sendMessage(View view){
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
+
+}
